f2e3d87bb8b11f314c920a2cac1d1171f1d586be
Admin <Admin@ADMIN>
2023-05-24 23:46:00 +0700

    - Done load image and show on app window wpf

--- APP_NTN_ELECTRONIC/MainWindow.xaml.cs
+++ APP_NTN_ELECTRONIC/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.Win32;
+using Emgu.Util.TypeEnum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
 
 namespace APP_NTN_ELECTRONIC
 {
@@ -23,6 +29,65 @@ namespace APP_NTN_ELECTRONIC
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetFullView8_8(byte[] input)
+        {
+            int row = 0;
+            int col = 0;
+            StackPanelImage.Children.Clear();
+            for (row = 0; row < 8; row++)
+            {
+                StackPanel stackPanelRow = new StackPanel();
+                stackPanelRow.Orientation = Orientation.Horizontal;
+                for (col = 0; col < 8; col++)
+                {
+                    byte blue = input[(col + row*8) * 3 + 0];
+                    byte green = input[(col + row*8) * 3 + 1];
+                    byte red = input[(col + row*8) * 3 + 2];
+                    SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(red,green, blue));
+                    Button button = new Button();
+                    button.Width = 50;
+                    button.Height = 50;
+                    button.Background = brush;
+                    stackPanelRow.Children.Add(button);
+                }
+                StackPanelImage.Children.Add(stackPanelRow);
+            }
+        }
+        private BitmapSource ConvertBitmapToBitmapSource(Bitmap bitmap)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+        private void bt_load_image_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string imagePath = openFileDialog.FileName;
+                Image<Bgr, byte> originalImage = new Image<Bgr, byte>(imagePath);
+
+                // Kích thước mới (chiều rộng và chiều cao) cho hình ảnh resize
+                int newWidth = 8;
+                int newHeight = 8;
+                // Resize hình ảnh
+                Image<Bgr, byte> resizedImage = originalImage.Resize(newWidth, newHeight, Emgu.CV.CvEnum.Inter.Linear);
+                byte[] imageData = resizedImage.Bytes;
+                imageControl.Source = ConvertBitmapToBitmapSource( resizedImage.ToBitmap()) ;
+                GetFullView8_8(imageData);
+            }
         }
     }
 }
